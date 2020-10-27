f3ce9217dc817ae7f61cebd008dd0ae8f2855572
mako4kamiya <mako4kamiya@gmail.com>
2020-10-28 00:15:27 +0900

    mongoDBの設定からUserモデルの作成、接続確認まで

--- server.js
+++ server.js
@@ -22,8 +22,24 @@ app.get("/", (req, res) => {
     res.json({ message: "Hello, World!" });
 });
 
+require("./app/routes/user.routes")(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
+});
+
+// Define Mongoose
+const db = require("./app/models");
+db.mongoose.connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log("Connected to the database!");
+})
+.catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
 });
