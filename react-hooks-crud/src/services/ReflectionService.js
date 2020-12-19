8cd9a05e36cb47759f4d4232a39c9eb86a5113a1
mako4kamiya <mako4kamiya@gmail.com>
2020-12-19 10:42:41 +0900

    59_Create React Components-AddReflection

--- react-hooks-crud/src/services/ReflectionService.js
+++ react-hooks-crud/src/services/ReflectionService.js
@@ -1,7 +1,8 @@
 import http from "../http-common";
 
 const getAll = () => {
-    return http.get(`/users/${id}/reflections`);
+    return http.get(`/users/5fdc2b3b33d89103b4dba73e/reflections`);
+    // return http.get(`/users/${id}/reflections`);
 };
 
 const get = id => {
@@ -9,7 +10,8 @@ const get = id => {
 };
 
 const create = data => {
-    return http.post(`/users/${id}/reflections`, data);
+    return http.post(`/users/5fdc2b3b33d89103b4dba73e/reflections`, data);
+    // return http.post(`/users/${id}/reflections`, data);
 };
 
 const update = (id, data) => {
@@ -21,7 +23,8 @@ const remove = id => {
 };
 
 const findToday = title => {
-    return http.get(`/reflections?Today=${createdAt}`);
+    return http.get(`/reflections?Today=2020-12-18T04:08:27.173Z`);
+    // return http.get(`/reflections?Today=${createdAt}`);
 };
 
 export default {
