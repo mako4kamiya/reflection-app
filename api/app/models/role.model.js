775e36264f99c81173930ad4a7d63f292941478a
mako4kamiya <mako4kamiya@gmail.com>
2021-03-06 01:14:37 +0900

    add Role model

---
+++ api/app/models/role.model.js
@@ -0,0 +1,10 @@
+module.exports = mongoose => {
+    const Role = mongoose.model(
+        "Role",
+        mongoose.Schema(
+        {
+            name: String
+        })
+    );
+    return Role;
+};
