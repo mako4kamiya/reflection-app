eb178161696a037aca5acca1d635e199c69beae8
mako4kamiya <mako4kamiya@gmail.com>
2021-03-17 19:05:39 +0900

    JWT Auth react終わり！

--- react-hooks-crud/src/App.js
+++ react-hooks-crud/src/App.js
@@ -1,57 +1,122 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-import UserLogin from "./components/UserComponent/UserLogin";
-import AddUser from "./components/UserComponent/AddUser";
-import User from "./components/UserComponent/User";
-import UsersList from "./components/UserComponent/UsersList";
-import AddReflection from "./components/ReflectionComponent/AddReflection";
-import Reflection from "./components/ReflectionComponent/Reflection";
+import AuthService from "./services/auth.service";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import BoardUser from "./components/BoardUser";
+import BoardModerator from "./components/BoardModerator";
+import BoardAdmin from "./components/BoardAdmin";
+
+// import UserLogin from "./components/UserComponent/UserLogin";
+// import AddUser from "./components/UserComponent/AddUser";
+// import User from "./components/UserComponent/User";
+// import UsersList from "./components/UserComponent/UsersList";
+// import AddReflection from "./components/ReflectionComponent/AddReflection";
+// import Reflection from "./components/ReflectionComponent/Reflection";
 
 function App() {
-  return (
+  const [showModeratorBoard, setShowModeratorBoard] = useState(false);
+  const [showAdminBoard, setShowAdminBoard] = useState(false);
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    const user = AuthService.getCurrentUser();
+    if (user) {
+      setCurrentUser(user);
+      setShowModeratorBoard(user.roles.includes("ROLE_MODERATOR"));
+      setShowAdminBoard(user.roles.includes("ROLE_ADMIN"));
+    }
+  }, []);
+
+  const logOut = () => {
+    AuthService.logout();
+  };
+
+return (
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <a href="/" className="navbar-brand">
+        <Link to={"/"} className="navbar-brand">
           Reflection App
-        </a>
+        </Link>
         <div className="navbar-nav mr-auto">
           <li className="nav-item">
-            <Link to={"/login"} className="nav-link">
-              Login
+            <Link to={"/home"} className="nav-link">
+              Home
             </Link>
           </li>
-          <li className="nav-item">
-            <Link to={"/signup"} className="nav-link">
-              Signup
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/users"} className="nav-link">
-              Users
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/add"} className="nav-link">
-              Add
-            </Link>
-          </li>
+
+          {showModeratorBoard && (
+            <li className="nav-item">
+              <Link to={"/mod"} className="nav-link">
+                Moderator Board
+              </Link>
+            </li>
+          )}
+
+          {showAdminBoard && (
+            <li className="nav-item">
+              <Link to={"/admin"} className="nav-link">
+                Admin Board
+              </Link>
+            </li>
+          )}
+
+          {currentUser && (
+            <li className="nav-item">
+              <Link to={"/user"} className="nav-link">
+                User
+              </Link>
+            </li>
+          )}
         </div>
+
+        {currentUser ? (
+          <div className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to={"/profile"} className="nav-link">
+                {currentUser.username}
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a href="/login" className="nav-link" onClick={logOut}>
+                LogOut
+              </a>
+            </li>
+          </div>
+        ) : (
+          <div className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to={"/login"} className="nav-link">
+                Login
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link to={"/register"} className="nav-link">
+                Sign Up
+              </Link>
+            </li>
+          </div>
+        )}
       </nav>
 
       <div className="container mt-3">
         <Switch>
-          <Route exact path="/signup" component={AddUser} />
-          <Route exact path="/users/:id" component={User} />
-          <Route exact path="/users" component={UsersList} />
-          <Route exact path="/add" component={AddReflection} />
-          <Route exact path="/reflections/:id" component={Reflection} />
+          <Route exact path={["/", "/home"]} component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/profile" component={Profile} />
+          <Route path="/user" component={BoardUser} />
+          <Route path="/mod" component={BoardModerator} />
+          <Route path="/admin" component={BoardAdmin} />
         </Switch>
       </div>
     </div>
   );
-}
-
+};
 export default App;
